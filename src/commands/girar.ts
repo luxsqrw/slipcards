be3f6e191c67efa52fd9c916c77f5828d051e02e
luxsqrw <lu1ziinho1870@gmail.com>
2024-06-04 12:18:33 -0300

    Atualizar o girar.ts

--- src/commands/girar.ts
+++ src/commands/girar.ts
@@ -5,25 +5,25 @@ export default async (ctx: BotContext) => {
   if (await _brklyn.cache.get('is_drawing', ctx.from?.id.toString())) {
     const url = await _brklyn.cache.get('is_drawing', ctx.from?.id.toString())
     if (url.startsWith) {
-      return ctx.reply('🕹 Você já está girando. Por favor, espere até que o giro atual termine.\n\nCaso a mensagem tenha sido deletada, use /cancelar para poder girar de novo.', {
+      return ctx.reply('🕹 Você já está rodando. Por favor, espere até que o giro atual termine.\n\nCaso a mensagem tenha sido deletada, use /cancelar para poder girar de novo.', {
         reply_markup: {
           inline_keyboard: [[{ text: '🔄 Ir à mensagem do giro', url: url }]]
         }
       })
     }
 
-    return ctx.reply('🕹 Você já está girando. Por favor, espere até que o giro atual termine.')
+    return ctx.reply('🕹 Você já está rodando. Por favor, espere até que o giro atual termine.')
   }
 
   if (ctx.userData.usedDraws >= ctx.userData.maximumDraws) {
-    return ctx.replyWithHTML('Ah... sinto muito, mas você já girou os cards que podia por agora. 😣\nVocê receberá mais um giros em ' + _brklyn.sidecar.willRunIn(DRAW_CRON) + '.\n\nP.S.: Você sabia que agora você pode comprar giros usando suas moedas? Use <code>/comprar giros quantidade</code>!')
+    return ctx.replyWithHTML('Ah... sinto muito, mas você já rodou os cards que podia por agora. 😁\nVocê receberá mais um spins em ' + _brklyn.sidecar.willRunIn(DRAW_CRON) + '.\n\nP.S.: Você sabia que agora você pode comprar spins usando suas moedas? Use <code>/comprar spin quantidade</code>!')
   }
 
   const cooldownDraws = await _brklyn.cache.get('draw_cooldowns', ctx.from?.id.toString()) || 0
   const groupCooldownDraws = await _brklyn.cache.get('draw_cooldowns', ctx.chat!.id.toString()) || 0
 
   if (cooldownDraws > 3 || groupCooldownDraws > 2) {
-    return ctx.reply('🕹 Aaah, que bagunça, estão girando muito rápido! Por favor, espere um pouco antes de girar novamente.')
+    return ctx.reply('🕹 Aaah, que bagunça, estão rodando muito rápido! Por favor, espere um pouco antes de rodar novamente.')
   }
 
   ctx.es2.enter('START_DRAW').catch(() => undefined)
@@ -31,5 +31,5 @@ export default async (ctx: BotContext) => {
 
 export const info = {
   guards: ['hasJoinedGroup', 'isWhitelistedGroup'],
-  aliases: ['draw', 'rodar', 'rechear']
+  aliases: ['draw', 'rodar', 'spin']
 }
